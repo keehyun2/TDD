db8ebd43aea6a1a14e631c8b04430b76bfaa134c
박기현 <keehyun2@gmail.com>
2023-04-02 17:49:59 +0900

    중복코드 함수 추출

--- src/test/java/ch03/ExpiryDateCalculatorTest.java
+++ src/test/java/ch03/ExpiryDateCalculatorTest.java
@@ -11,22 +11,15 @@ class ExpiryDateCalculatorTest {
     @Test
     void 납부일자_납부금액_전달받아서_만료일자반환(){
         // 10_000 당 한달 연장
-        LocalDate billingDate = LocalDate.of(2019, 3, 1);
-        int payAmount = 10_000;
-
-        ExpiryDateCalculator cal = new ExpiryDateCalculator();
-        LocalDate expiryDate = cal.calculateExpiryDate(billingDate, payAmount);
-
-        assertEquals(LocalDate.of(2019, 4, 1), expiryDate);
+        assertExpiryDate(LocalDate.of(2019,3,1), 10_000, LocalDate.of(2019,4,1));
 
         // 다른 납부일자 case 추가
-        LocalDate billingDate2 = LocalDate.of(2019, 5, 5);
-        int payAmount2 = 10_000;
+        assertExpiryDate(LocalDate.of(2019,5,5), 10_000, LocalDate.of(2019,6,5));
+    }
 
-        ExpiryDateCalculator cal2 = new ExpiryDateCalculator();
-        LocalDate expiryDate2 = cal2.calculateExpiryDate(billingDate2, payAmount2);
-
-        assertEquals(LocalDate.of(2019, 6, 5), expiryDate2);
-
+    private void assertExpiryDate(LocalDate billingDate, int payAmount, LocalDate expectedExpiryDate){
+        ExpiryDateCalculator expiryDateCalculator = new ExpiryDateCalculator();
+        LocalDate realExpiryDate = expiryDateCalculator.calculateExpiryDate(billingDate, payAmount);
+        assertEquals(realExpiryDate, expectedExpiryDate);
     }
 }
