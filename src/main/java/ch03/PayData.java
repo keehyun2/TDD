ad980dfc0e09be41178c149fa96f9793223433dd
박기현 <keehyun2@gmail.com>
2023-04-02 18:51:05 +0900

    firstBillingDate 추가 (최초 납부일자)

--- src/main/java/ch03/PayData.java
+++ src/main/java/ch03/PayData.java
@@ -4,11 +4,17 @@ import java.time.LocalDate;
 
 public class PayData {
     private LocalDate billingDate;
+    private LocalDate firstBillingDate;
     private int payAmount;
 
-    public PayData(LocalDate billingDate, int payAmount) {
+    public PayData(LocalDate firstBillingDate, LocalDate billingDate, int payAmount) {
+        this.firstBillingDate = firstBillingDate;
         this.billingDate = billingDate;
         this.payAmount = payAmount;
+    }
+
+    public LocalDate getFirstBillingDate() {
+        return firstBillingDate;
     }
 
     public LocalDate getBillingDate() {
@@ -20,9 +26,14 @@ public class PayData {
     }
 
     public static class Builder {
-
+        private LocalDate firstBillingDate;
         private LocalDate billingDate;
         private int payAmount;
+
+        public Builder setFirstBillingDate(LocalDate firstBillingDate) {
+            this.firstBillingDate = firstBillingDate;
+            return this;
+        }
 
         public Builder setBillingDate(LocalDate billingDate) {
             this.billingDate = billingDate;
@@ -35,7 +46,7 @@ public class PayData {
         }
 
         public PayData createPayData() {
-            return new PayData(billingDate, payAmount);
+            return new PayData(firstBillingDate, billingDate, payAmount);
         }
     }
 
