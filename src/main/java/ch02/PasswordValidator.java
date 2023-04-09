ba0c5f508c52afb19c3aa1fe44f5ea637063af21
박기현 <keehyun2@gmail.com>
2023-04-09 17:35:44 +0900

    업무 규칙정리

--- src/main/java/ch02/PasswordValidator.java
+++ src/main/java/ch02/PasswordValidator.java
@@ -3,7 +3,12 @@ package ch02;
 public class PasswordValidator {
 
     /**
-     * 
+     * - 길이 8글자 이상
+     * - 0부터 9사이 숫자를 초함
+     * - 대문자 포함
+     * 3개 규칙을 충족하면 암호는 강함
+     * 2개 규칙을 충족하면 암호는 보통
+     * 0~1 이하의 규칙을 충족하면 암호는 약함이다.
      * @param password 패스워드
      * @return 패스워드 강도
      */
